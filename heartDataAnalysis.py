c379436e563bb83fd81a56008c91e4fd1bf1b529
EndiMuceku <endim2015@gmail.com>
2020-12-19 23:02:19 +0000

    improvements to kNN and random forest models

--- heartDataAnalysis.py
+++ heartDataAnalysis.py
@@ -62,11 +62,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2)
 
 #build a knn model
-knn = KNeighborsClassifier(n_neighbors=16)
+knn = KNeighborsClassifier(n_neighbors=52, weights='uniform')
 knn.fit(X_train, y_train) 
 
 #Instantiate the model with 10 trees and entropy as splitting criteria
-rf = RandomForestClassifier()
+rf = RandomForestClassifier(n_estimators = 40)
 rf.fit(X_train, y_train)
 
 #make predictions
